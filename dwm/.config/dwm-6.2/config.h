6bfec6f34a941221376cdfab4c46dd318860bac9
EasterEggo <phsb0607@protonmail.com>
2021-07-16 09:38:00 -0300

    added my dwm configs

---
+++ dwm/.config/dwm-6.2/config.h
@@ -0,0 +1,133 @@
+/* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>
+/* appearance */
+static const unsigned int borderpx = 1; /* border pixel of windows */
+static const unsigned int snap = 32;    /* snap pixel */
+static const unsigned int gappx = 5;    /* gaps between windows */
+static const int user_bh = 25; /*0 means that dwm will calculate bar height, >=
+                                  1 means dwm will user_bh as bar height */
+static const char term[] = "st"; /* default terminal */
+static const int showbar = 1;    /* 0 means no bar */
+static const int topbar = 1;     /* 0 means bottom bar */
+static const char *fonts[] = {"Ubuntu Nerd Font:size=11:weight=bold"};
+static const char dmenufont[] = "Ubuntu Nerd Font:size=11:weight=bold";
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#444444";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_cyan[] = "#005577";
+static const char *colors[][3] = {
+    /*               fg         bg         border   */
+    [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
+    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+};
+
+/* tagging */
+static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+static const Rule rules[] = {
+    /* xprop(1):
+     *	WM_CLASS(STRING) = instance, class
+     *	WM_NAME(STRING) = title
+     */
+    /* class      instance    title       tags mask     isfloating   monitor */
+    {"Gimp", NULL, NULL, 0, 1, -1},
+    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
+};
+
+/* layout(s) */
+static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;    /* number of clients in master area */
+static const int resizehints =
+    1; /* 1 means respect size hints in tiled resizals */
+
+static const Layout layouts[] = {
+    {"[]=", tile}, /* default */
+    {"><>", NULL}, /* no floating behavior */
+    {"[M]", monocle},
+};
+
+/* key definitions */
+#define MODKEY Mod4Mask
+#define TAGKEYS(KEY, TAG)                                                      \
+  {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
+      {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
+      {MODKEY | ShiftMask, KEY, tag, {.ui = 1 << TAG}},                        \
+      {MODKEY | ControlMask | ShiftMask, KEY, toggletag, {.ui = 1 << TAG}},
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd)                                                             \
+  {                                                                            \
+    .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
+  }
+
+/* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *termcmd[] = {term};
+static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *browser[] = {"firefox", NULL };
+static const char *textEditor[] = {"emacs", NULL };
+static const char *files[] = {term, "-e", "ranger", NULL };
+static const char *youtube[] = {"ytfzf", "-D", NULL };
+static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
+static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
+static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
+static const char *pausecmd[] = {"playerctl", "play-pause"};
+static Key keys[] = {
+    /*keybindings*/
+
+    /*Launch Programs*/	
+    { MODKEY, XK_p, spawn, {.v = dmenucmd} },
+    { MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd} },
+    { MODKEY, XK_b, spawn, {.v = browser} },
+    { MODKEY, XK_e, spawn, {.v = textEditor} },
+    { MODKEY, XK_f, spawn, {.v = files} },
+    { MODKEY, XK_y, spawn, {.v = youtube} },
+    /*Media Keys*/
+    { 0, XF86XK_AudioMute, spawn, {.v = mutecmd } },
+    { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
+    { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
+    { 0, XF86XK_AudioPlay, spawn, {.v = pausecmd}},
+    /*General*/
+    { MODKEY | ShiftMask, XK_b, togglebar, {0} },
+    { MODKEY, XK_j, focusstack, {.i = +1} },
+    { MODKEY, XK_k, focusstack, {.i = -1} },
+    { MODKEY, XK_i, incnmaster, {.i = +1} },
+    { MODKEY, XK_d, incnmaster, {.i = -1} },
+    { MODKEY, XK_h, setmfact, {.f = -0.05} },
+    { MODKEY, XK_l, setmfact, {.f = +0.05} },
+    { MODKEY, XK_Return, zoom, {0} },
+    { MODKEY, XK_Tab, view, {0} },
+    {MODKEY | ShiftMask, XK_c, killclient, {0} },
+    { MODKEY, XK_t, setlayout, {.v = &layouts[0]} },
+    { MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]} },
+    { MODKEY, XK_m, setlayout, {.v = &layouts[2]} },
+    { MODKEY, XK_space, setlayout, {0} },
+    { MODKEY | ShiftMask, XK_space, togglefloating, {0} },
+    { MODKEY, XK_0, view, {.ui = ~0} },
+    { MODKEY | ShiftMask, XK_0, tag, {.ui = ~0} },
+    { MODKEY, XK_comma, focusmon, {.i = -1} },
+    { MODKEY, XK_period, focusmon, {.i = +1} },
+    { MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1} },
+    { MODKEY | ShiftMask, XK_period, tagmon, {.i = +1} },
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
+        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
+            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0} },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
+ * ClkClientWin, or ClkRootWin */
+static Button buttons[] = {
+    /* click|vent|mask|button|function argument*/
+    {ClkLtSymbol, 0, Button1, setlayout, {0}},
+    {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
+    {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
+    {ClkClientWin, MODKEY, Button1, movemouse, {0}},
+    {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
+    {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
+    {ClkTagBar, 0, Button1, view, {0}},
+    {ClkTagBar, 0, Button3, toggleview, {0}},
+    {ClkTagBar, MODKEY, Button1, tag, {0}},
+    {ClkTagBar, MODKEY, Button3, toggletag, {0}},
+};
