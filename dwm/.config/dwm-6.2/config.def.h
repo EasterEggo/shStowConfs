6949ced429fdd0f0f97ebbf016f2275280037e1f
EasterEggo <phsb0607@protonmail.com>
2021-08-09 01:53:36 -0300

    updated my dwm config

--- dwm/.config/dwm-6.2/config.def.h
+++ dwm/.config/dwm-6.2/config.def.h
@@ -1,16 +1,20 @@
-/* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
+#define print 0x0000ff61
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const unsigned int gappx = 5;    /* gaps between windows */
+
+static const unsigned int gappx = 5; /* gaps between windows */
 static const int user_bh = 25; /*0 means that dwm will calculate bar height, >=
                                   1 means dwm will user_bh as bar height */
-static const char term[] = "st"; /* default terminal */
-static const int showbar = 1;    /* 0 means no bar */
-static const int topbar = 1;     /* 0 means bottom bar */
+static const char term[] = "alacritty"; /* default terminal */
+
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
+
 static const char *fonts[] = {"Ubuntu Nerd Font:size=11:weight=bold"};
 static const char dmenufont[] = "Ubuntu Nerd Font:size=11:weight=bold";
+
 static const char col_gray1[] = "#222222";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
@@ -32,7 +36,6 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor */
     {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
 };
 
 /* layout(s) */
@@ -62,57 +65,67 @@ static const Layout layouts[] = {
   }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] =
+    "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *termcmd[] = {term};
-static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *browser[] = {"firefox", NULL };
-static const char *textEditor[] = {"emacs", NULL };
-static const char *files[] = {term, "-e", "ranger", NULL };
-static const char *youtube[] = {"ytfzf", "-D", NULL };
-static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
-static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
-static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
-static const char *pausecmd[] = {"playerctl", "play-pause"};
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+static const char *roficmd[] = {"rofi",         "-show",  "run",
+                                "-display-run", "Run:  ", NULL};
+static const char *browser[] = {"firefox", NULL};
+static const char *textEditor[] = {"emacs", NULL};
+static const char *files[] = {term, "-e", "ranger", NULL};
+static const char *youtube[] = {"ytfzf", "-D", NULL};
+
+static const char *mutecmd[] = {"amixer", "-q", "set", "PCM", "toggle", NULL};
+static const char *volupcmd[] = {"amixer", "-q",     "set", "PCM",
+                                 "5%+",    "unmute", NULL};
+static const char *voldowncmd[] = {"amixer", "-q",     "set", "PCM",
+                                   "5%-",    "unmute", NULL};
+static const char *pausecmd[] = {"playerctl", "play-pause", NULL};
+static const char *scrotcmd[] = {"scrot", NULL};
 static Key keys[] = {
     /*keybindings*/
 
-    /*Launch Programs*/	
-    { MODKEY, XK_p, spawn, {.v = dmenucmd} },
-    { MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd} },
-    { MODKEY, XK_b, spawn, {.v = browser} },
-    { MODKEY, XK_e, spawn, {.v = textEditor} },
-    { MODKEY, XK_f, spawn, {.v = files} },
-    { MODKEY, XK_y, spawn, {.v = youtube} },
+    /*Launch Programs*/
+    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
+    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_b, spawn, {.v = browser}},
+    {MODKEY, XK_e, spawn, {.v = textEditor}},
+    {MODKEY, XK_f, spawn, {.v = files}},
+    {MODKEY, XK_y, spawn, {.v = youtube}},
+    {0, print, spawn, {.v = scrotcmd}},
     /*Media Keys*/
-    { 0, XF86XK_AudioMute, spawn, {.v = mutecmd } },
-    { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
-    { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
-    { 0, XF86XK_AudioPlay, spawn, {.v = pausecmd}},
+    {0, XF86XK_AudioMute, spawn, {.v = mutecmd}},
+    {0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd}},
+    {0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd}},
+    {0, XF86XK_AudioPlay, spawn, {.v = pausecmd}},
     /*General*/
-    { MODKEY | ShiftMask, XK_b, togglebar, {0} },
-    { MODKEY, XK_j, focusstack, {.i = +1} },
-    { MODKEY, XK_k, focusstack, {.i = -1} },
-    { MODKEY, XK_i, incnmaster, {.i = +1} },
-    { MODKEY, XK_d, incnmaster, {.i = -1} },
-    { MODKEY, XK_h, setmfact, {.f = -0.05} },
-    { MODKEY, XK_l, setmfact, {.f = +0.05} },
-    { MODKEY, XK_Return, zoom, {0} },
-    { MODKEY, XK_Tab, view, {0} },
-    {MODKEY | ShiftMask, XK_c, killclient, {0} },
-    { MODKEY, XK_t, setlayout, {.v = &layouts[0]} },
-    { MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]} },
-    { MODKEY, XK_m, setlayout, {.v = &layouts[2]} },
-    { MODKEY, XK_space, setlayout, {0} },
-    { MODKEY | ShiftMask, XK_space, togglefloating, {0} },
-    { MODKEY, XK_0, view, {.ui = ~0} },
-    { MODKEY | ShiftMask, XK_0, tag, {.ui = ~0} },
-    { MODKEY, XK_comma, focusmon, {.i = -1} },
-    { MODKEY, XK_period, focusmon, {.i = +1} },
-    { MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1} },
-    { MODKEY | ShiftMask, XK_period, tagmon, {.i = +1} },
+    {MODKEY | ShiftMask, XK_b, togglebar, {0}},
+    {MODKEY, XK_j, focusstack, {.i = +1}},
+    {MODKEY, XK_k, focusstack, {.i = -1}},
+    {MODKEY, XK_i, incnmaster, {.i = +1}},
+    {MODKEY, XK_d, incnmaster, {.i = -1}},
+    {MODKEY, XK_h, setmfact, {.f = -0.05}},
+    {MODKEY, XK_l, setmfact, {.f = +0.05}},
+    {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY, XK_Tab, view, {0}},
+    {MODKEY | ShiftMask, XK_c, killclient, {0}},
+    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
+    {MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]}},
+    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+    {MODKEY, XK_space, setlayout, {0}},
+    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+    {MODKEY, XK_0, view, {.ui = ~0}},
+    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+    {MODKEY, XK_comma, focusmon, {.i = -1}},
+    {MODKEY, XK_period, focusmon, {.i = +1}},
+    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
+    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0} },
+            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
--- dwm/.config/dwm-6.2/config.h
+++ dwm/.config/dwm-6.2/config.h
@@ -1,25 +1,25 @@
-/* See LICENSE file for copyright and license details. */
 #include <X11/XF86keysym.h>
+#define print 0x0000ff61
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
 static const unsigned int snap = 32;    /* snap pixel */
 
-static const unsigned int gappx = 5;    /* gaps between windows */
+static const unsigned int gappx = 5; /* gaps between windows */
 static const int user_bh = 25; /*0 means that dwm will calculate bar height, >=
                                   1 means dwm will user_bh as bar height */
 static const char term[] = "alacritty"; /* default terminal */
 
-static const int showbar = 1;    /* 0 means no bar */
-static const int topbar = 1;     /* 0 means bottom bar */
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
 
 static const char *fonts[] = {"Ubuntu Nerd Font:size=11:weight=bold"};
 static const char dmenufont[] = "Ubuntu Nerd Font:size=11:weight=bold";
 
-static const char col_gray1[] = "#222222";
+static const char col_gray1[] = "#02111B";
 static const char col_gray2[] = "#444444";
 static const char col_gray3[] = "#bbbbbb";
 static const char col_gray4[] = "#eeeeee";
-static const char col_cyan[] = "#005577";
+static const char col_cyan[] = "#073C5F";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray3, col_gray1, col_gray2},
@@ -27,7 +27,7 @@ static const char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+static const char *tags[] = {" ", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -36,7 +36,7 @@ static const Rule rules[] = {
      */
     /* class      instance    title       tags mask     isfloating   monitor */
     {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 8, 0, -1},
+    {"Firefox", NULL, 0, 0, 0, -1},
 };
 
 /* layout(s) */
@@ -46,9 +46,9 @@ static const int resizehints =
     1; /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
-    {"[]=", tile}, /* default */
-    {"><>", NULL}, /* no floating behavior */
-    {"[M]", monocle},
+    {"", tile}, /* default */
+    {"", NULL}, /* no floating behavior */
+    {"", monocle},
 };
 
 /* key definitions */
@@ -66,59 +66,67 @@ static const Layout layouts[] = {
   }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] =
+    "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *termcmd[] = {term};
-static const char *dmenucmd[] = {"dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *roficmd[] = {"rofi", "-show", "run", "-display-run", "Run:  ", NULL };
-static const char *browser[] = {"firefox", NULL };
-static const char *textEditor[] = {"emacs", NULL };
-static const char *files[] = {term, "-e", "ranger", NULL };
-static const char *youtube[] = {"ytfzf", "-D", NULL };
+static const char *dmenucmd[] = {
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+static const char *roficmd[] = {"rofi",         "-show",  "run",
+                                "-display-run", "Run:  ", NULL};
+static const char *browser[] = {"firefox", NULL};
+static const char *textEditor[] = {"emacs", NULL};
+static const char *files[] = {term, "-e", "ranger", NULL};
+static const char *youtube[] = {"ytfzf", "-D", NULL};
 
-static const char *mutecmd[] = { "amixer", "-q", "set", "Master", "toggle", NULL };
-static const char *volupcmd[] = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
-static const char *voldowncmd[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
-static const char *pausecmd[] = {"playerctl", "play-pause"};
+static const char *mutecmd[] = {"amixer", "-q", "set", "PCM", "toggle", NULL};
+static const char *volupcmd[] = {"amixer", "-q",     "set", "PCM",
+                                 "5%+",    "unmute", NULL};
+static const char *voldowncmd[] = {"amixer", "-q",     "set", "PCM",
+                                   "5%-",    "unmute", NULL};
+static const char *pausecmd[] = {"playerctl", "play-pause", NULL};
+static const char *scrotcmd[] = {"scrot", NULL};
 static Key keys[] = {
     /*keybindings*/
 
-    /*Launch Programs*/	
-    { MODKEY, XK_p, spawn, {.v = roficmd} },
-    { MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd} },
-    { MODKEY, XK_b, spawn, {.v = browser} },
-    { MODKEY, XK_e, spawn, {.v = textEditor} },
-    { MODKEY, XK_f, spawn, {.v = files} },
-    { MODKEY, XK_y, spawn, {.v = youtube} },
+    /*Launch Programs*/
+    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
+    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
+    {MODKEY, XK_b, spawn, {.v = browser}},
+    {MODKEY, XK_e, spawn, {.v = textEditor}},
+    {MODKEY, XK_f, spawn, {.v = files}},
+    {MODKEY, XK_y, spawn, {.v = youtube}},
+    {0, print, spawn, {.v = scrotcmd}},
     /*Media Keys*/
-    { 0, XF86XK_AudioMute, spawn, {.v = mutecmd } },
-    { 0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd } },
-    { 0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd } },
-    { 0, XF86XK_AudioPlay, spawn, {.v = pausecmd}},
+    {0, XF86XK_AudioMute, spawn, {.v = mutecmd}},
+    {0, XF86XK_AudioLowerVolume, spawn, {.v = voldowncmd}},
+    {0, XF86XK_AudioRaiseVolume, spawn, {.v = volupcmd}},
+    {0, XF86XK_AudioPlay, spawn, {.v = pausecmd}},
     /*General*/
-    { MODKEY | ShiftMask, XK_b, togglebar, {0} },
-    { MODKEY, XK_j, focusstack, {.i = +1} },
-    { MODKEY, XK_k, focusstack, {.i = -1} },
-    { MODKEY, XK_i, incnmaster, {.i = +1} },
-    { MODKEY, XK_d, incnmaster, {.i = -1} },
-    { MODKEY, XK_h, setmfact, {.f = -0.05} },
-    { MODKEY, XK_l, setmfact, {.f = +0.05} },
-    { MODKEY, XK_Return, zoom, {0} },
-    { MODKEY, XK_Tab, view, {0} },
-    {MODKEY | ShiftMask, XK_c, killclient, {0} },
-    { MODKEY, XK_t, setlayout, {.v = &layouts[0]} },
-    { MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]} },
-    { MODKEY, XK_m, setlayout, {.v = &layouts[2]} },
-    { MODKEY, XK_space, setlayout, {0} },
-    { MODKEY | ShiftMask, XK_space, togglefloating, {0} },
-    { MODKEY, XK_0, view, {.ui = ~0} },
-    { MODKEY | ShiftMask, XK_0, tag, {.ui = ~0} },
-    { MODKEY, XK_comma, focusmon, {.i = -1} },
-    { MODKEY, XK_period, focusmon, {.i = +1} },
-    { MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1} },
-    { MODKEY | ShiftMask, XK_period, tagmon, {.i = +1} },
+    {MODKEY | ShiftMask, XK_b, togglebar, {0}},
+    {MODKEY, XK_j, focusstack, {.i = +1}},
+    {MODKEY, XK_k, focusstack, {.i = -1}},
+    {MODKEY, XK_i, incnmaster, {.i = +1}},
+    {MODKEY, XK_d, incnmaster, {.i = -1}},
+    {MODKEY, XK_h, setmfact, {.f = -0.05}},
+    {MODKEY, XK_l, setmfact, {.f = +0.05}},
+    {MODKEY, XK_Return, zoom, {0}},
+    {MODKEY, XK_Tab, view, {0}},
+    {MODKEY | ShiftMask, XK_c, killclient, {0}},
+    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
+    {MODKEY | ShiftMask, XK_f, setlayout, {.v = &layouts[1]}},
+    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
+    {MODKEY, XK_space, setlayout, {0}},
+    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
+    {MODKEY, XK_0, view, {.ui = ~0}},
+    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+    {MODKEY, XK_comma, focusmon, {.i = -1}},
+    {MODKEY, XK_period, focusmon, {.i = +1}},
+    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
+    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0} },
+            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
 };
 
 /* button definitions */
