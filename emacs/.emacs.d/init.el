57561d9a41ec2a23af82669e12136fd8c72fda69
EasterEggo <phsb0607@protonmail.com>
2021-08-26 21:50:03 -0300

    updated my emacs configs and added org file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -2,23 +2,20 @@
 (setq backup-directory-alist '(("" . "~/.emacs.d/backups"))
       delete-old-versions t)
 
-;;straight plugin manager setup
 (defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+	(let ((bootstrap-file
+		    (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+		   (bootstrap-version 5))
+	       (unless (file-exists-p bootstrap-file)
+		 (with-current-buffer
+		     (url-retrieve-synchronously
+		      "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+		      'silent 'inhibit-cookies)
+		   (goto-char (point-max))
+		   (eval-print-last-sexp)))
+	       (load bootstrap-file nil 'nomessage))
 (setq straight-use-package-by-default t)
 
-
-;;packages using straight.el
 (straight-use-package 'use-package)
 (straight-use-package 'el-patch)
 (straight-use-package 'ivy)
@@ -34,21 +31,17 @@
 (straight-use-package 'magit)
 (straight-use-package 'doom-modeline)
 (straight-use-package 'which-key)
+(straight-use-package 'password-store)
+(straight-use-package 'gruvbox-theme)
 (straight-use-package 'toc-org)
 (straight-use-package 'slime)
 
-
-;;theme
 (load-theme 'chocolate t)
 
-
-;;removing menubars
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 
-
-;;welcome screen
 (use-package dashboard
   :config
   (dashboard-setup-startup-hook)
@@ -56,12 +49,9 @@
    initial-buffer-choice (lambda () (get-buffer "*dashboard*"))
    dashboard-set-heading-icons t
    dashboard-set-file-icons t
-   dashboard-banner-logo-title "Welcome to Teamacs"
    dashboard-startup-banner 'official
    dashboard-center-content t))
 
-
-;;tabs
 (use-package centaur-tabs
   :config
   (centaur-tabs-mode t)
@@ -76,14 +66,10 @@
   (vterm-mode . centaur-tabs-local-mode)
   (dashboard-mode . centaur-tabs-local-mode))
 
-
-;;LSP
 (use-package lsp-mode
   :hook
   (rust-mode . lsp))
 
-
-;;activating plugins
 (setq inferior-lisp-program "sbcl")
 (evil-mode 1)
 (ivy-mode 1)
@@ -91,22 +77,16 @@
 (add-hook 'after-init-hook #'doom-modeline-mode)
 (add-hook 'after-init-hook 'global-company-mode)
 
-
-;;plugins config
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 (which-key-setup-side-window-bottom)
 (which-key-setup-minibuffer)
 (which-key-mode)
 
-
-;;bindings
 (global-set-key [f8] 'neotree-toggle)
 (evil-set-leader 'normal (kbd "<SPC>"))
 (define-key evil-normal-state-map (kbd "<leader>.") 'dired)
 (define-key evil-normal-state-map (kbd "<leader>SPC") 'execute-extended-command)
 
-
-;;org mode
 (setq
  org-directory "~/organization"
  org-agenda-files "~/organization/agenda.org"
@@ -125,10 +105,10 @@
   (org-roam-directory (file-truename "~/organization/roam/"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-	 ("C-c n g" . org-roam-graph)
-	 ("C-c n i" . org-roam-node-insert)
-	 ("C-c n c" . org-roam-capture)
-	 ("C-c n j" . org-roam-dailies-capture-today))
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ("C-c n j" . org-roam-dailies-capture-today))
   :config
   (org-roam-db-autosync-mode))
 
@@ -143,9 +123,8 @@
           org-roam-ui-open-on-start t))
 (straight-use-package 'websocket)
 
-;;fonts
 (set-face-attribute 'default nil :font "Fira Code Nerd Font-13" )
-;; set font for emoji
+
 (set-fontset-font
  t
  '(#x1f300 . #x1fad0)
@@ -156,6 +135,5 @@
   ((member "Symbola" (font-family-list)) "Symbola")
   ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji")))
 
-;;misc
 (defalias 'yes-or-no-p 'y-or-n-p)
 (org-roam-db-sync 1)
