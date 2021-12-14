df3e303d983f6db199b364fb1765a946e2b8b296
EasterEggo <phsb0607@protonmail.com>
2021-12-14 09:09:59 -0300

    added keybindings for the password store and organized some packages

--- emacs/.emacs.d/cfg/prod.el
+++ emacs/.emacs.d/cfg/prod.el
@@ -6,28 +6,35 @@
   (require 'org-roam-protocol)
   (setq org-roam-protocol-store-links "~/org/roam")
   :general
-  (:states 'normal "<SPC>ni" 'org-roam-node-insert))
+  (:states 'normal
+	   "<SPC>ni" 'org-roam-node-insert))
 
 (use-package org-bullets :ghook 'org-mode-hook)
 
 (use-package org
-  :gfhook ('org-mode-hook
-	   (list #'org-indent-mode)))
+  :gfhook ('org-mode-hook (list #'org-indent-mode))
+  :config (setq org-agenda-files '("~/org/roam/20211129102234-agenda.org")))
 
-(use-package pass :config (use-package password-store))
+(use-package pass
+  :config (use-package password-store)
+  :general
+  (:states 'normal
+	   :prefix "<SPC>p"
+	   "p" 'pass
+	   "i" 'password-store-insert
+	   "r" 'password-store-rename
+	   "d" 'password-store-remove
+	   "g" 'password-store-generate))
 
 (use-package perspective
   :init (persp-mode)
-  :general (:states 'normal
-		    :prefix "<SPC>p"
-
-		    "s" 'persp-switch
-		    "d" 'persp-kill
-		    "r" 'persp-rename))
-
+  :general
+  (:states 'normal
+	   :prefix "<SPC>w"
+	   "s" 'persp-switch
+	   "d" 'persp-kill
+	   "r" 'persp-rename))
 
 (use-package projectile :init (projectile-mode))
-
-(setq org-agenda-files '("~/org/roam/20211129102234-agenda.org"))
 
 (provide 'prod)
