d194a81dd267997a8b7cd09cccf8c1e2c4f66914
EasterEggo <phsb0607@tutanota.com>
2021-12-16 10:39:08 -0300

    added some search result indicators integrated in doom modeline

--- emacs/.emacs.d/cfg/style.el
+++ emacs/.emacs.d/cfg/style.el
@@ -6,18 +6,23 @@
 (tool-bar-mode -1)
 
 (use-package doom-themes :init (load-theme 'doom-monokai-octagon t))
-(use-package doom-modeline :init (doom-modeline-mode))
-(use-package anzu :init (global-anzu-mode) :config (use-package evil-anzu))
 
-(use-package rainbow-delimiters :ghook ('prog-mode-hook))
-(use-package rainbow-mode :ghook ('prog-mode-hook))
+(use-package doom-modeline :init (doom-modeline-mode))
+
+(use-package anzu :init (global-anzu-mode))
+
+(use-package evil-anzu :requires (anzu))
+
+(use-package rainbow-delimiters :ghook 'prog-mode-hook)
+
+(use-package rainbow-mode :ghook 'prog-mode-hook)
 
 (use-package sublimity)
 (require 'sublimity-scroll)
 
 (use-package format-all
   :gfhook ('prog-mode-hook (list #'format-all-mode #'format-all-ensure-formatter))
-  :config (setq format-all-formatters t))
+  :custom (format-all-formatters t))
 
 ;;(set-frame-parameter (selected-frame) 'alpha '(95 . 50))
 ;;(add-to-list 'default-frame-alist '(alpha . (95 . 50)))
