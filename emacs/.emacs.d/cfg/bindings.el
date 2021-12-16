3aacac1165d4f2c95429ffbcb9895c9891242586
EasterEggo <phsb0607@tutanota.com>
2021-12-16 10:21:18 -0300

    formated to evil* packages to use-package format

--- emacs/.emacs.d/cfg/bindings.el
+++ emacs/.emacs.d/cfg/bindings.el
@@ -1,10 +1,14 @@
 (use-package general)
 
 (use-package evil
-  :init (evil-mode)
-  :config (use-package evil-nerd-commenter
-	    :general
-	    (:states 'normal "<SPC>;" 'evilnc-comment-or-uncomment-lines)))
+  :init
+  (evil-mode))
+
+(use-package evil-nerd-commenter
+  :requires
+  (evil)
+  :general
+  (:states 'normal "<SPC>;" 'evilnc-comment-or-uncomment-lines))
 
 (use-package which-key :init (which-key-mode))
 
