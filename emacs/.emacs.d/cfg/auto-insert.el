94a68002d4690404efcebe78b7b699d0e9ed57a7
EasterEggo <phsb0607@tutanota.com>
2021-12-16 10:19:45 -0300

    formated yasnippet to use-package format

--- emacs/.emacs.d/cfg/auto-insert.el
+++ emacs/.emacs.d/cfg/auto-insert.el
@@ -1,15 +1,17 @@
 (setq auto-insert-directory "~/.emacs.d/cfg/snippets")
 
-(use-package yasnippet)
-(setq yas-snippet-dirs '("~/.emacs.d/cfg/snippets"))
-(yas-global-mode 1)
+(use-package yasnippet
+  :init
+  (yas-global-mode)
+  :custom
+  (setq yas-snippet-dirs '("~/.emacs.d/cfg/snippets")))
 
 (auto-insert-mode)
 (setq auto-insert-query nil)
 
 (defun autoinsert-yas-expand()
-      "Replace text in yasnippet template."
-      (yas/expand-snippet (buffer-string) (point-min) (point-max)))
+  "Replace text in yasnippet template."
+  (yas/expand-snippet (buffer-string) (point-min) (point-max)))
 
 (setq auto-insert-alist '(
 			  (("\\.c\\'" . "c template") . ["c-mode/__c" autoinsert-yas-expand])
