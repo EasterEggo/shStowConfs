b034f7f1c1cd46f40ed69b6f2c8da5c2680f7519
EasterEggo <phsb0607@tutanota.com>
2021-12-19 17:23:41 -0300

    added two new keybindings("/" and "C-.") and prettified code

--- emacs/.emacs.gnu/cfg/completion.el
+++ emacs/.emacs.gnu/cfg/completion.el
@@ -7,33 +7,41 @@
   embark
   embark-consult
   t
+
   :init
   (vertico-mode)
   (savehist-mode)
   (marginalia-mode)
   (all-the-icons-completion-mode)
+
   :custom
   (completion-styles '(orderless))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion))))
   :general
+
   (:states 'normal
 	   :prefix "<SPC>"
 	   "<SPC>" 'execute-extended-command
 	   "." 'find-file
+
 	   "<" 'consult-buffer
 	   "b" 'consult-bookmark
 	   "r" 'consult-recent-file)
-  (:states 'normal
-	   "/" 'consult-line))
+
+  (:states 'normal "/" 'consult-line)
+
+  ("C-." 'embark-act))
 
 (use-package company
   :straight company-box t
+
   :custom
   (global-company-mode 1)
   (company-idle-delay 0)
   (company-minimum-prefix-length 1)
   (company-selection-wrap-around t)
+
   :ghook
   ('company-mode-hook #'company-box-mode))
 
