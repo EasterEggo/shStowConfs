0b9acb930e6536edee979467bb53254706bb9c3a
EasterEggo <phsb0607@tutanota.com>
2021-12-19 17:25:24 -0300

    unified evil and evilnc

--- emacs/.emacs.gnu/cfg/keys.el
+++ emacs/.emacs.gnu/cfg/keys.el
@@ -1,17 +1,13 @@
 (use-package general)
 
 (use-package evil
+  :straight evil-nerd-commenter t
   :init
   (evil-mode)
   :general
   (:states 'normal
-	   "/" 'swiper))
-
-(use-package evil-nerd-commenter
-  :requires
-  (evil)
-  :general
-  (:states 'normal "<SPC>;" 'evilnc-comment-or-uncomment-lines))
+	   "/" 'consult-line
+	   "<SPC>;" 'evilnc-comment-or-uncomment-lines))
 
 (use-package which-key :init (which-key-mode))
 
