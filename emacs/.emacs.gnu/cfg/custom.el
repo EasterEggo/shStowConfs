bb95236aa1404c6766c28585b2c13a219f0eb9b9
EasterEggo <phsb0607@tutanota.com>
2021-12-23 17:09:41 -0300

    removed 'custom' file and put the file's content inside init.el

--- emacs/.emacs.gnu/cfg/custom.el
+++
@@ -1,3 +0,0 @@
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(provide 'custom)
--- emacs/.emacs.gnu/init.el
+++ emacs/.emacs.gnu/init.el
@@ -1,5 +1,8 @@
 (add-to-list 'load-path  (expand-file-name "cfg/" user-emacs-directory))
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 (setq make-backup-files nil)
 
 (require 'straight)
