756183110a567811e97f85dd2be937fe7020ef35
EasterEggo <phsb0607@tutanota.com>
2022-01-20 11:56:55 -0300

    added vertico-directory related bindings and added company delay

--- emacs/.emacs.gnu/cfg/completion.el
+++ emacs/.emacs.gnu/cfg/completion.el
@@ -1,24 +1,24 @@
 (use-package vertico
-:straight
-consult
-marginalia
-savehist orderless
-all-the-icons-completion
-embark
-embark-consult
-t
+ :straight
+ consult
+ marginalia
+ savehist orderless
+ all-the-icons-completion
+ embark
+ embark-consult
+ t
 
-:init
-(vertico-mode)
-(savehist-mode)
-(marginalia-mode)
-(all-the-icons-completion-mode)
+ :init
+ (vertico-mode)
+ (savehist-mode)
+ (marginalia-mode)
+ (all-the-icons-completion-mode)
 
-:custom
-(completion-styles '(orderless))
-(completion-category-defaults nil)
-(completion-category-overrides '((file (styles partial-completion))))
-:general
+ :custom
+ (completion-styles '(orderless))
+ (completion-category-defaults nil)
+ (completion-category-overrides '((file (styles partial-completion))))
+ :general
 
 (:states 'normal
          :prefix "<SPC>"
@@ -31,14 +31,18 @@ t
 
 (:states 'normal "/" 'consult-line)
 
-("C-." 'embark-act))
+("C-." 'embark-act)
+(:keymaps 'vertico-map
+ "<DEL>" 'vertico-directory-delete-char))
+
+(add-to-list 'load-path (expand-file-name "straight/repos/vertico/extensions" user-emacs-directory))
+(require 'vertico-directory)
 
 (use-package company
 :straight company-box t
 
 :custom
 (global-company-mode 1)
-(company-idle-delay 0)
 (company-minimum-prefix-length 1)
 (company-selection-wrap-around t)
 
