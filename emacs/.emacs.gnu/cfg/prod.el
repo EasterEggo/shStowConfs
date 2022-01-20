5a213f42a34c1ae21b52d41a54d3572282868c91
EasterEggo <phsb0607@tutanota.com>
2022-01-20 11:45:11 -0300

    removed org roam ui comments

--- emacs/.emacs.gnu/cfg/prod.el
+++ emacs/.emacs.gnu/cfg/prod.el
@@ -14,15 +14,11 @@
     :straight
     (:host github :repo "org-roam/org-roam-ui" :branch "main" :files ("*.el" "out"))
     :after org-roam
-  ;;normally we'd recommend hooking orui after org-roam, but since org-roam does not have
-  ;;a hookable mode anymore, you're advised to pick something yourself
-  ;;if you don't care about startup time, use
-  ;;:hook (after-init . org-roam-ui-mode)
-  :config
-  (setq org-roam-ui-sync-theme t
-        org-roam-ui-follow t
-        org-roam-ui-update-on-save t
-        org-roam-ui-open-on-start t))
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
 
 (use-package org-bullets :ghook 'org-mode-hook)
 
