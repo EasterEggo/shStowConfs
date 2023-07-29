f2fb8457b2739e09cc1a2638d52d2aab23d3d422
EasterEggo <phsb0607@tutanota.com>
2023-07-28 21:20:38 -0300

    [EMACS] switched to evil and general and used gc cons to speed up

--- private_dot_emacs.d/init.el
+++ private_dot_emacs.d/init.el
@@ -5,9 +5,17 @@
 (global-display-line-numbers-mode)
 (recentf-mode)
 (setq inhibit-startup-screen t)
-(set-face-attribute 'default nil :font "CommitMono" :height 100)
+(set-face-attribute 'default nil :font "CommitMono" :height 110)
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq make-backup-files nil)
+
+;; Minimize garbage collection during startup
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; Lower threshold back to 8 MiB (default is 800kB)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (expt 2 23))))
 
 (setq package-enable-at-startup nil)
 (defvar elpaca-installer-version 0.5)
@@ -51,6 +59,9 @@
   (setq elpaca-use-package-by-default t))
 (elpaca-wait)
 
+(use-package general :after emacs)
+(elpaca-wait)
+
 (use-package nerd-icons)
 (use-package modus-themes
   :config
@@ -63,90 +74,99 @@
 	(comment yellow-cooler)
         (string green-cooler)))
   (load-theme 'modus-vivendi t))
-(use-package meow
-  :config
-  (defun meow-setup ()
-    (setq meow-cheatsheet-layout meow-cheatsheet-layout-dvorak)
-    (meow-leader-define-key
-     '("1" . meow-digit-argument)
-     '("2" . meow-digit-argument)
-     '("3" . meow-digit-argument)
-     '("4" . meow-digit-argument)
-     '("5" . meow-digit-argument)
-     '("6" . meow-digit-argument)
-     '("7" . meow-digit-argument)
-     '("8" . meow-digit-argument)
-     '("9" . meow-digit-argument)
-     '("0" . meow-digit-argument)
-     '("/" . meow-keypad-describe-key)
-     '("?" . meow-cheatsheet))
-    (meow-motion-overwrite-define-key
-     ;; custom keybinding for motion state
-     '("<escape>" . ignore))
-    (meow-normal-define-key
-     '("0" . meow-expand-0)
-     '("9" . meow-expand-9)
-     '("8" . meow-expand-8)
-     '("7" . meow-expand-7)
-     '("6" . meow-expand-6)
-     '("5" . meow-expand-5)
-     '("4" . meow-expand-4)
-     '("3" . meow-expand-3)
-     '("2" . meow-expand-2)
-     '("1" . meow-expand-1)
-     '("-" . negative-argument)
-     '(";" . meow-reverse)
-     '("," . meow-inner-of-thing)
-     '("." . meow-bounds-of-thing)
-     '("<" . meow-beginning-of-thing)
-     '(">" . meow-end-of-thing)
-     '("a" . meow-append)
-     '("A" . meow-open-below)
-     '("b" . meow-back-word)
-     '("B" . meow-back-symbol)
-     '("c" . meow-change)
-     '("d" . meow-delete)
-     '("D" . meow-backward-delete)
-     '("e" . meow-line)
-     '("E" . meow-goto-line)
-     '("f" . meow-find)
-     '("g" . meow-cancel-selection)
-     '("G" . meow-grab)
-     '("h" . meow-left)
-     '("H" . meow-left-expand)
-     '("i" . meow-insert)
-     '("I" . meow-open-above)
-     '("j" . meow-join)
-     '("k" . meow-kill)
-     '("l" . meow-till)
-     '("m" . meow-mark-word)
-     '("M" . meow-mark-symbol)
-     '("t" . meow-next)
-     '("T" . meow-next-expand)
-     '("o" . meow-block)
-     '("O" . meow-to-block)
-     '("n" . meow-prev)
-     '("N" . meow-prev-expand)
-     '("q" . meow-quit)
-     '("Q" . meow-goto-line)
-     '("r" . meow-replace)
-     '("R" . meow-swap-grab)
-     '("p" . meow-search)
-     '("s" . meow-right)
-     '("S" . meow-right-expand)
-     '("u" . meow-undo)
-     '("U" . meow-undo-in-selection)
-     '("v" . meow-visit)
-     '("w" . meow-next-word)
-     '("W" . meow-next-symbol)
-     '("x" . meow-save)
-     '("X" . meow-sync-grab)
-     '("y" . meow-yank)
-     '("z" . meow-pop-selection)
-     '("'" . repeat)
-     '("<escape>" . ignore)))
-  (meow-setup)
-  (meow-global-mode 1))
+
+(use-package evil :config (evil-mode 1))
+
+
+;; (use-package meow
+;;   :config
+;;   (defun meow-setup ()
+;;     (setq meow-cheatsheet-layout meow-cheatsheet-layout-dvorak)
+;;     (meow-leader-define-key
+;;      '("1" . meow-digit-argument)
+;;      '("2" . meow-digit-argument)
+;;      '("3" . meow-digit-argument)
+;;      '("4" . meow-digit-argument)
+;;      '("5" . meow-digit-argument)
+;;      '("6" . meow-digit-argument)
+;;      '("7" . meow-digit-argument)
+;;      '("8" . meow-digit-argument)
+;;      '("9" . meow-digit-argument)
+;;      '("0" . meow-digit-argument)
+;;      '("/" . meow-keypad-describe-key)
+;;      '("?" . meow-cheatsheet))
+;;     (meow-motion-overwrite-define-key
+;;      ;; custom keybinding for motion state
+;;      '("<escape>--" . ignore))
+;;     (meow-normal-define-key
+;;      '("0" . meow-expand-0)
+;;      '("9" . meow-expand-9)
+;;      '("8" . meow-expand-8)
+;;      '("7" . meow-expand-7)
+;;      '("6" . meow-expand-6)
+;;      '("5" . meow-expand-5)
+;;      '("4" . meow-expand-4)
+;;      '("3" . meow-expand-3)
+;;      '("2" . meow-expand-2)
+;;      '("1" . meow-expand-1)
+;;      '("-" . negative-argument)
+;;      '(";" . meow-reverse)
+;;      '("," . meow-inner-of-thing)
+;;      '("." . meow-bounds-of-thing)
+;;      '("<" . meow-beginning-of-thing)
+;;      '(">" . meow-end-of-thing)
+;;      '("a" . meow-append)
+;;      '("A" . meow-open-below)
+;;      '("b" . meow-back-word)
+;;      '("B" . meow-back-symbol)
+;;      '("c" . meow-change)
+;;      '("d" . meow-delete)
+;;      '("D" . meow-backward-delete)
+;;      '("e" . meow-line)
+;;      '("E" . meow-goto-line)
+;;      '("f" . meow-find)
+;;      '("g" . meow-cancel-selection)
+;;      '("G" . meow-grab)
+;;      '("h" . meow-left)
+;;      '("H" . meow-left-expand)
+;;      '("i" . meow-insert)
+;;      '("I" . meow-open-above)
+;;      '("j" . meow-join)
+;;      '("k" . meow-kill)
+;;      '("l" . meow-till)
+;;      '("m" . meow-mark-word)
+;;      '("M" . meow-mark-symbol)
+;;      '("t" . meow-next)
+;;      '("T" . meow-next-expand)
+;;      '("o" . meow-block)
+;;      '("O" . meow-to-block)
+;;      '("n" . meow-prev)
+;;      '("N" . meow-prev-expand)
+;;      '("q" . meow-quit)
+;;      '("Q" . meow-goto-line)
+;;      '("r" . meow-replace)
+;;      '("R" . meow-swap-grab)
+;;      '("p" . meow-search)
+;;      '("s" . meow-right)
+;;      '("S" . meow-right-expand)
+;;      '("u" . meow-undo)
+;;      '("U" . meow-undo-in-selection)
+;;      '("v" . meow-visit)
+;;      '("w" . meow-next-word)
+;;      '("W" . meow-next-symbol)
+;;      '("x" . meow-save)
+;;      '("X" . meow-sync-grab)
+;;      '("y" . meow-yank)
+;;      '("z" . meow-pop-selection)
+;;      '("'" . repeat)
+;;      '("<escape>--" . ignore)))
+;;   (meow-setup)
+;;   (meow-global-mode 1))
+
+;; (use-package xah-fly-keys
+;; :init (xah-fly-keys 1)
+;; :config (xah-fly-keys-set-layout "programmer-dvorak"))
+
 
 (use-package vertico
   :elpaca (:files (:defaults "extensions/*"))
@@ -161,10 +181,13 @@
 (use-package embark-consult)
 (use-package embark)
 (use-package consult
-  :bind (("C-c o m" . consult-man)
-	 ("C-c o b" . consult-buffer)
-	 ("C-c o r" . consult-recent-file)
-	 ("C-c o g" . consult-ripgrep)))
+  :general
+  (:states 'normal
+	   :prefix "<SPC>o"
+	   "m" 'consult-man
+	   "b" 'consult-buffer
+	   "r" 'consult-recent-file
+	   "g" 'consult-ripgrep))
 (use-package marginalia
   :after vertico
   :config (marginalia-mode))
@@ -192,18 +215,21 @@
 (use-package diff-hl :hook (elpaca-after-init . global-diff-hl-mode))
 (use-package doom-modeline
   :init (doom-modeline-mode 1))
-(use-package rainbow-delimiters :hook (prog-mode . rainbow-delimiters-mode))
+
 (use-package org-modern
   :hook (org-mode . org-modern-mode))
 (use-package org-roam
   :custom
   (org-roam-directory (file-truename "~/roam"))
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n g" . org-roam-graph)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n c" . org-roam-capture)
-         ("C-c n j" . org-roam-dailies-capture-today))
+  :general
+  (:states 'normal
+	   :prefix "<SPC>n"
+	   "l" 'org-roam-buffer-toggle
+           "f" 'org-roam-node-find
+           "g" 'org-roam-graph
+           "i" 'org-roam-node-insert
+           "c" 'org-roam-capture
+           "j" 'org-roam-dailies-capture-today)
   :config
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode))
@@ -217,4 +243,6 @@
                            (lsp-deferred)
                            (platformio-conditionally-enable)))
 (use-package vterm
-  :bind (("C-c t" . vterm-other-window)))
+  :general
+  (:states 'normal
+	   "<SPC>t" 'vterm-other-window))
