071978ce60e5dc86d7ac9060c5b04f62beb2fd12
easteregg <phsb0607@tutanota.com>
2025-02-10 14:45:47 -0300

    removed wrong dirs

--- .config/nvim/init.lua
+++
@@ -1,5 +0,0 @@
-vim.g.mapleader = " "
-require("cfg.options")
-require("cfg.lazy")
-require("cfg.mason")
-require("cfg.completion")
--- .config/nvim/lua/plugins/editing.lua
+++
@@ -1,9 +0,0 @@
-return{
-{
-  'stevearc/oil.nvim',
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-},
-vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "Open parent directory" }),
-}
-
